a4f254442797bf1b44eaaadc3ad788ab9619e903
devatquarxss <devatquarxss@gmail.com>
2022-11-04 01:36:21 +0530

    first commit

--- ios/Plugin/rpPlugin.swift
+++ ios/Plugin/rpPlugin.swift
@@ -1,18 +1,42 @@
 import Foundation
 import Capacitor
+import Razorpay
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
+
+//class ViewController: UIViewController, RazorpayPaymentCompletionProtocol, WKNavigationDelegate {
+//        func onPaymentSuccess(_ payment_id: String, andData response: [AnyHashable : Any]) {
+//            <#code#>
+//        }
+//
+//        func onPaymentError(_ code: Int32, description str: String, andData response: [AnyHashable : Any]) {
+//            <#code#>
+//        }
+//
+//    // typealias Razorpay = RazorpayCheckout
+//        var webView: WKWebView!
+//        var razorpay: RazorpayCheckout!
+//
+//        override func viewDidLoad() {
+//            super.viewDidLoad()
+//            self.webView = WKWebView(frame: self.view.frame)
+//            self.razorpay = RazorpayCheckout.initWithKey("Key", andDelegate: self, withPaymentWebView: self.webView)
+//            self.view.addSubview(self.webView)
+//        }
+//}
+
 @objc(rpPlugin)
 public class rpPlugin: CAPPlugin {
     private let implementation = rp()
 
     @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
+
+        //let value = call.getString("value") ?? ""
         call.resolve([
-            "value": implementation.echo(value)
+            "value": implementation.echo("cat_dog_wolf")
         ])
     }
 }
